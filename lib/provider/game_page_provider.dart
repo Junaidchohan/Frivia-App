b89fc7467b8bfcc48a21589c7e1e0ba788a6c75e
Junaidchohane <junaidchohanedu@gmail.com>
2026-01-06 15:45:04 +0500

    Tracking Score

--- lib/provider/game_page_provider.dart
+++ lib/provider/game_page_provider.dart
@@ -7,6 +7,7 @@ class GamePageProvider with ChangeNotifier {
 
   List? questions;
   int currentQuestionIndex = 0;
+  int correctAccount = 0;
 
   // Add your state variables and methods here to manage game state
   BuildContext? context;
@@ -46,5 +47,57 @@ class GamePageProvider with ChangeNotifier {
         textAlign: TextAlign.center,
       );
     }
+  }
+
+  void answerQuestion(String answer) async {
+    // Handle the answer logic here
+    // For example, check if the answer is correct and update the score
+    // Then fetch the next question or end the game if maxQuestions reached
+    bool isCorrect =
+        questions![currentQuestionIndex]['correct_answer'] == answer;
+    correctAccount += isCorrect ? 1 : 0;
+    currentQuestionIndex++;
+    showDialog(
+      context: context!,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: isCorrect ? Colors.green : Colors.red,
+          title: Icon(
+            isCorrect ? Icons.check_circle : Icons.cancel,
+            color: Colors.white,
+            size: 48,
+          ),
+        );
+      },
+    );
+    Future.delayed(Duration(seconds: 1), () {
+      Navigator.of(context!).pop();
+      if (currentQuestionIndex >= maxQuestions) {
+        endGame();
+      } else {
+        notifyListeners();
+      }
+    });
+  }
+
+  Future<void> endGame() async {
+    // Handle end game logic here
+    // For example, show final score and reset the game state
+    showDialog(
+      context: context!,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: Colors.blue,
+          title: Text('Game Over!', style: TextStyle(color: Colors.white)),
+          content: Text(
+            "Score: $correctAccount / $maxQuestions",
+            style: TextStyle(color: Colors.white),
+          ),
+        );
+      },
+    );
+    await Future.delayed(Duration(seconds: 2));
+    Navigator.of(context!).pop(); // Close the dialog
+    currentQuestionIndex = 0;
   }
 }
